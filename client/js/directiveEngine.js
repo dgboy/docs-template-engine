f36851ffc9f75f523f0c20756a0b32c6afc069f2
dgboy <dan.gorbunov.00@yandex.ru>
2020-06-01 15:58:04 +0300

    fix

--- client/js/directiveEngine.js
+++ client/js/directiveEngine.js
@@ -1,8 +1,5 @@
 docsTemplateEngine.directive('docsTemplateEngine', () => {
   return {
-    link: function (scope, element, attrs) {
-      scope.data = scope[attrs["docsTemplateEngine"]];
-    },
     restrict: "A",
     template: "<div class='row justify-content-between'>" +
       "<div class='col-4 ml-3'>" +
@@ -11,18 +8,17 @@ docsTemplateEngine.directive('docsTemplateEngine', () => {
       "<li class='list-group-item list-group-item-action' ng-repeat='t in templates'" +
       "ng-class='{active : cur.templateName === t.name}' ng-click='chooseTemplate(t)'>" +
       "<div>{{t.name}}</div>" +
-      "<div class='p-1' ng-if='cur.templateName === t.name'>" +
+      "<div ng-if='cur.templateName === t.name'>" +
       "<hr>" +
       "<textarea class='form-control col' rows='5' ng-model='cur.data'></textarea>" +
-      "<button class='mx-1 my-1 btn btn-success' ng-click='updateData(cur.data)'>Изменить</button>" +
+      "<button class='mx-1 my-1 btn btn-warning' ng-click='updateData(cur.data)'>Применить</button>" +
       "<textarea class='form-control col' rows='10' ng-model='cur.template'></textarea>" +
-      "<button class='mx-1 my-1 btn btn-success' ng-click='updateTemplate(cur.template)'>Изменить</button>" +
+      "<button class='mx-1 my-1 btn btn-warning' ng-click='updateTemplate(cur.template)'>Применить</button>" +
       "<div class='mx-auto my-1 custom-file'>" +
       "<input type='file' class='custom-file-input' id='imageFile' ng-click='loadBackground(t)'>" +
       "<label class='custom-file-label' for='imageFile'>Загрузить фон...</label>" +
       "</div>" +
-      "<button class='col btn btn-success my-1' ng-class='{disabled : !cur.data}' ng-click='createDocs(t)'>Создать" +
-      "экземпляры</button>" +
+      "<button class='col btn btn-success my-1' ng-class='{disabled : !cur.data}' ng-click='createDocs(t)'>Создать экземпляры</button>" +
       "<div class='row justify-content-between my-1' ng-show='cur.data'>" +
       "<button class='mx-1 btn btn-secondary' data-slide='prev' ng-click='changeDataset(-1)'><</button> " +
       "<span class='col pt-3 badge badge-dark'>Текущий набор: {{cur.dataset}}</span>" +
